701089c91f0dd91d86570e35fa8b187a472de6b5
a30a <github@a30a.dev>
2020-04-18 23:57:10 +0900

    Remove unused meta data

--- src/@lekoarts/gatsby-theme-minimal-blog/components/seo.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/seo.tsx
@@ -34,27 +34,17 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    url: `${siteUrl}${pathname || ``}`,
-    image: `${siteUrl}${image || defaultImage}`,
+    url: `${siteUrl}${pathname || ``}`
   }
   return (
     <Helmet title={title} defaultTitle={defaultTitle} titleTemplate={`%s | ${siteTitle}`}>
       <html lang={siteLanguage} />
       <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
       <meta property="og:title" content={seo.title} />
       <meta property="og:url" content={seo.url} />
       <meta property="og:description" content={seo.description} />
-      <meta property="og:image" content={seo.image} />
       <meta property="og:type" content="website" />
       <meta property="og:image:alt" content={seo.description} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={seo.title} />
-      <meta name="twitter:url" content={seo.url} />
-      <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />
-      <meta name="twitter:image:alt" content={seo.description} />
-      <meta name="twitter:creator" content={author} />
       <meta name="gatsby-theme" content="@lekoarts/gatsby-theme-minimal-blog" />
       {children}
     </Helmet>
